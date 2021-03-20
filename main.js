fbb5f08dfe227c6239f4c7001ef4f174abe97237
Marie Fedinová <marie.fedinova@gmail.com>
2021-03-20 12:53:35 +0100

    Vypracovaný domácí úkol č. 2 - třídy a události.

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
 // tady je místo pro náš program
+
+/**
+ * Funkce pro ztučnění textu po najetí myši
+ * @param {string} udalost 
+ */
+
+function ztučni(udalost) {
+    udalost.target.style.fontWeight = "bold";
+}
+
+function ztenči(udalost) {
+    udalost.target.style.fontWeight = "normal";
+}
+
+/**
+ * Funkce pro přidání css třídy červená a obarvení odstavce
+ */
+
+let odstavec = document.querySelector('p')
+
+function změňBarvu() {
+    odstavec.classList.toggle("červená");
+}
+
+/**
+ * Funkce pro zvětšení textu vždy o 1px
+ */
+
+function zvětši() {
+    let size = parseFloat(odstavec.style.fontSize);
+    let plusOne = size + 1;
+    odstavec.style.fontSize = plusOne + "px";
+}
+
+/**
+ * Funkce, která přehraje audio
+ * @param {string} element 
+ */
+
+function přehraj(element) {
+    let zvuk = document.querySelector(element);
+    zvuk.play();
+}
+
+/**
+ * Funkce pro změnu hlasitosti audia
+ * @param {number} number
+ */
+
+let audio = document.querySelector('#audio');
+
+function změňHlasitost(number) {
+    audio.volume = number;
+}
+
+/**
+ * Funkce, která přehraje audio od začátku
+ * @param {string} element 
+ */
+
+function přehrajOdZačátku(element) {
+    let zvuk = document.querySelector(element);
+    zvuk.load();
+    zvuk.play();
+}
